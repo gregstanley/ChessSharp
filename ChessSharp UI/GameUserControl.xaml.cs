b7bdddb630d8f477f99e1c07c02b025100c92dde
Greg Stanley <24586962+gregstanley@users.noreply.github.com>
2018-11-26 21:51:05 +0000

    Moving final bits of logic into a GameUserControl

---
+++ ChessSharp UI/GameUserControl.xaml.cs
@@ -0,0 +1,60 @@
+﻿using ChessSharp.Engine;
+using ChessSharp.Engine.Events;
+using ChessSharp.Enums;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ChessSharp_UI
+{
+    public partial class GameUserControl : UserControl
+    {
+        public Game _game;
+
+        public GameUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public void NewGameWhite(Game game)
+        {
+            _game = game;
+
+            HalfTurnNumberLabel.Content = _game.FullTurnNumber;
+
+            BoardUserControl.Load(_game, _game.GetGameState());
+        }
+
+        public Task NewGameBlack(Game game)
+        {
+            _game = game;
+
+            HalfTurnNumberLabel.Content = _game.FullTurnNumber;
+
+            BoardUserControl.Load(_game, _game.GetGameState());
+
+            return DoSearch();
+        }
+
+        private Task DoSearch() => _game.CpuMove(3);
+
+        private async void BoardUserControl_PieceMoved(object sender, UserMovedPieceEventArgs args) =>
+            await OnPieceMoved(args); 
+
+        private async void BoardUserControl_PieceSelected(object sender, PromotionTypeSelectedEventArgs args) =>
+            await OnPieceSelected(args);
+
+        private Task OnPieceMoved(UserMovedPieceEventArgs args)
+        {
+            var move = _game.TryMove(args.FromSquareIndex, args.ToSquareIndex, PieceType.None);
+
+            return move.Value == 0 ? Task.CompletedTask : DoSearch();
+        }
+
+        private Task OnPieceSelected(PromotionTypeSelectedEventArgs args)
+        {
+            var move = _game.TryMove(args.FromSquareIndex, args.ToSquareIndex, args.PieceType);
+
+            return move.Value == 0 ? Task.CompletedTask : DoSearch();
+        }
+    }
+}
--- ChessSharp UI/MainWindow.xaml.cs
+++ ChessSharp UI/MainWindow.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using ChessSharp;
 using ChessSharp.Engine;
-using ChessSharp.Engine.Events;
 using ChessSharp.Enums;
-using ChessSharp.Extensions;
-using CSharpFunctionalExtensions;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -12,59 +8,21 @@ namespace ChessSharp_UI
 {
     public partial class MainWindow : Window
     {
-        private Game _currentGame;
-
-        private IReadOnlyCollection<ImageMap> _images { get; set; }
-
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void BtnNewGameWhite_Click(object sender, RoutedEventArgs e) => NewGameWhite();
+        private void BtnNewGameWhite_Click(object sender, RoutedEventArgs e) =>
+            NewGameWhite();
 
-        private async void BtnNewGameBlack_Click(object sender, RoutedEventArgs e) => await NewGameBlack();
+        private async void BtnNewGameBlack_Click(object sender, RoutedEventArgs e) =>
+            await NewGameBlack();
 
-        private async Task NewGameBlack()
-        {
-            var bitBoard = new BitBoard();
+        private void NewGameWhite() =>
+            GameUserControl.NewGameWhite(new Game(new BitBoard(), Colour.White));
 
-            _currentGame = new Game(bitBoard, Colour.Black);
-
-            BoardUserControl.Load(_currentGame, _currentGame.GetGameState());
-
-            await DoSearch();
-        }
-
-        private void NewGameWhite()
-        {
-            var bitBoard = new BitBoard();
-
-            _currentGame = new Game(bitBoard, Colour.White);
-
-            BoardUserControl.Load(_currentGame, _currentGame.GetGameState());
-        }
-
-        private Task DoSearch() => _currentGame.CpuMove(3);
-
-        private void BoardUserControl_PieceMoved(object sender, UserMovedPieceEventArgs args)
-        {
-            var move = _currentGame.TryMove(args.FromSquareIndex, args.ToSquareIndex, PieceType.None);
-
-            if (move.Value == 0)
-                return;
-
-            DoSearch();
-        }
-
-        private void BoardUserControl_PieceSelected(object sender, PromotionTypeSelectedEventArgs args)
-        {
-            var move = _currentGame.TryMove(args.FromSquareIndex, args.ToSquareIndex, args.PieceType);
-
-            if (move.Value == 0)
-                return;
-
-            DoSearch();
-        }
+        private Task NewGameBlack() =>
+            GameUserControl.NewGameBlack(new Game(new BitBoard(), Colour.Black));
     }
 }
